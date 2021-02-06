569a9f79d0b606219c4a29c57c1f05b6761f8c57
faidlurrohman <faidrohman@outlook.com>
2021-02-07 04:00:36 +0700

    for getting all data images

---
+++ routes/controllers/allImage.js
@@ -0,0 +1,39 @@
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+const db = require("../../services/dbConnect");
+
+// cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+exports.allImage = (request, response) => {
+  // data from user
+  const { cloudinary_id } = request.params;
+
+  db.pool.connect((err, client) => {
+    // query to find image
+    const query = "SELECT * FROM images";
+
+    // execute query
+    client
+      .query(query)
+      .then((output) => {
+        response.status(200).send({
+          status: "success",
+          data: output.rows,
+        });
+      })
+      .catch((error) => {
+        response.status(401).send({
+          status: "failure",
+          data: {
+            message: "could not retrieve record!",
+            error,
+          },
+        });
+      });
+  });
+};
