2b9d035666c61a9b6184d69f86f006b4d4a9ad75
faidlurrohman <faidrohman@outlook.com>
2021-02-07 02:58:17 +0700

    init

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const uploadImage = require("./controllers/uploadImage");
+const getImage = require("./controllers/getImage");
+const deleteImage = require("./controllers/deleteImage");
+const updateImage = require("./controllers/updateImage");
+
+router.get("/", (request, response) => {
+  response.json({ message: "Hey! This is your server response!" });
+});
+
+router.post("/upload-image", uploadImage.uploadImage);
+router.get("/retrieve-image/:cloudinary_id", getImage.getImage);
+router.delete("/delete-image/:cloudinary_id", deleteImage.deleteImage);
+router.put("/update-image/:cloudinary_id", updateImage.updateImage);
+
+module.exports = router;
