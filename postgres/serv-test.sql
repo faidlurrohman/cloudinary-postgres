d083db0c66fa3eca88fc60e4cbf983a035f8969f
faidlurrohman <faidrohman@outlook.com>
2021-02-07 04:20:12 +0700

    update

--- postgres/serv-test.sql
+++ postgres/serv-test.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.5 (Ubuntu 12.5-0ubuntu0.20.04.1)
 -- Dumped by pg_dump version 12.5 (Ubuntu 12.5-0ubuntu0.20.04.1)
 
--- Started on 2021-02-07 03:21:01 WIB
+-- Started on 2021-02-07 04:16:41 WIB
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -78,6 +78,8 @@ ALTER TABLE ONLY public.images ALTER COLUMN id SET DEFAULT nextval('public.image
 
 COPY public.images (id, title, cloudinary_id, image_url) FROM stdin;
 5	walaah	fusjnuyqiuew1lnzdwix	https://res.cloudinary.com/df0idyfu5/image/upload/v1612640874/fusjnuyqiuew1lnzdwix.jpg
+6	walaah	zsk5wuqmvjbmnkuene3v	https://res.cloudinary.com/df0idyfu5/image/upload/v1612644797/zsk5wuqmvjbmnkuene3v.jpg
+7	walaah	odxqs5ii4rluvnytdfot	https://res.cloudinary.com/df0idyfu5/image/upload/v1612645942/odxqs5ii4rluvnytdfot.jpg
 \.
 
 
@@ -87,7 +89,7 @@ COPY public.images (id, title, cloudinary_id, image_url) FROM stdin;
 -- Name: images_id_seq; Type: SEQUENCE SET; Schema: public; Owner: serv-test
 --
 
-SELECT pg_catalog.setval('public.images_id_seq', 5, true);
+SELECT pg_catalog.setval('public.images_id_seq', 7, true);
 
 
 --
@@ -99,7 +101,7 @@ ALTER TABLE ONLY public.images
     ADD CONSTRAINT images_pkey PRIMARY KEY (id);
 
 
--- Completed on 2021-02-07 03:21:02 WIB
+-- Completed on 2021-02-07 04:16:41 WIB
 
 --
 -- PostgreSQL database dump complete
