97a1dba33e01b4679e84a1a50a74f152181e9faf
faidlurrohman <faidrohman@outlook.com>
2021-02-07 04:34:59 +0700

    remove unnecessary

--- routes/controllers/allImage.js
+++ routes/controllers/allImage.js
@@ -1,18 +1,6 @@
-const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
 const db = require("../../services/dbConnect");
 
-// cloudinary configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
 exports.allImage = (request, response) => {
-  // data from user
-  const { cloudinary_id } = request.params;
-
   db.pool.connect((err, client) => {
     // query to find image
     const allQuery = "SELECT * FROM images";
