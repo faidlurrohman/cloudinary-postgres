7efccece355f997b60981fc9c1f02fc7dde22efa
faidlurrohman <faidrohman@outlook.com>
2021-02-07 04:00:27 +0700

    new route and renaming old route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const allImage = require("./controllers/allImage");
 const uploadImage = require("./controllers/uploadImage");
 const getImage = require("./controllers/getImage");
 const deleteImage = require("./controllers/deleteImage");
@@ -10,8 +11,9 @@ router.get("/", (request, response) => {
   response.json({ eating: "HAMburger!" });
 });
 
+router.get("/all-images", allImage.allImage);
 router.post("/upload-image", uploadImage.uploadImage);
-router.get("/retrieve-image/:cloudinary_id", getImage.getImage);
+router.get("/get-image/:cloudinary_id", getImage.getImage);
 router.delete("/delete-image/:cloudinary_id", deleteImage.deleteImage);
 router.put("/update-image/:cloudinary_id", updateImage.updateImage);
 
